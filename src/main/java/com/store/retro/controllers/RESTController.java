f96a84c5af01d03beb89f2dd2e0ffd04c6ff9265
Tarell Morris <tarellamorris@gmail.com>
2025-12-23 18:45:19 -0500

    feat(app): nginx added for reverse proxy

--- src/main/java/com/store/retro/controllers/RESTController.java
+++ src/main/java/com/store/retro/controllers/RESTController.java
@@ -1,12 +1,12 @@
 package com.store.retro.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RESTController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "Hello World from the backend";
     }
