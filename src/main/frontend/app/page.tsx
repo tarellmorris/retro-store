7ca8c0653519bf682fee89d5bbe4c43ead668441
Tarell Morris <tarellamorris@gmail.com>
2025-12-26 16:02:44 -0500

    fix(next): hmr working in docker dev mode

--- src/main/frontend/app/page.tsx
+++ src/main/frontend/app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
       <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
           <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-            <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-teal-600 dark:text-purple-400">
+            <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-red-600 dark:text-purple-400">
               Updated wording from the backend: {message}
             </h1>
           </div>
