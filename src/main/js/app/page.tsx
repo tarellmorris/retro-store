8bfe48e8c4895ae2e43f402d0af9591f9ba4088e
Tarell Morris <tarellamorris@gmail.com>
2025-12-26 10:59:22 -0500

    chore(app): moving next around

--- src/main/js/app/page.tsx
+++ src/main/js/app/page.tsx
@@ -2,6 +2,7 @@ export const dynamic = "force-dynamic";
 
 export default async function Home() {
 	let message = "";
+
 	try {
 		const response = await fetch("http://nginx:80/api/");
 		if (!response.ok) {
@@ -16,8 +17,8 @@ export default async function Home() {
 		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 			<main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
 				<div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-					<h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-						Message from the backend: {message}
+					<h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-pink-600 dark:text-purple-400">
+						Updated wording from the backend: {message}
 					</h1>
 				</div>
 			</main>
