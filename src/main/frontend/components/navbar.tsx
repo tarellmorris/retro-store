caeb8598bc379f0265ef549dc0d142c1c166579c
Tarell Morris <tarellamorris@gmail.com>
2026-01-27 15:35:01 -0500

    feat(frontend): login redirect added to nvbar

--- src/main/frontend/components/navbar.tsx
+++ src/main/frontend/components/navbar.tsx
@@ -3,19 +3,33 @@ import Link from "next/link";
 
 export const NavBar = () => {
   return (
-    <nav className="flex w-screen p-4 pl-8 pr-8 bg-amber-500 gap-4 items-center font-sans drop-shadow-sm">
-      <Link href="/">
-        <div className="flex gap-4 items-center">
+    <nav className="flex w-screen p-4 pl-8 pr-8 bg-amber-500 gap-4 items-center justify-between font-sans drop-shadow-sm">
+      <div>
+        <Link href="/">
+          <div className="flex gap-4 items-center">
+            <Image
+              alt="logo"
+              height={40}
+              preload={true}
+              src={"/tv.svg"}
+              width={40}
+            />
+            <span className="text-xl font-bold text-zinc-50">Retro Store</span>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <Link className="flex gap-2 items-center" href="/login">
+          <span className="text-medium font-bold text-zinc-50">Login</span>
           <Image
-            alt="logo"
-            height={40}
+            alt="user"
+            height={25}
             preload={true}
-            src={"/tv.svg"}
-            width={40}
+            src={"/user.svg"}
+            width={25}
           />
-          <span className="text-xl font-bold text-zinc-50">Retro Store</span>
-        </div>
-      </Link>
+        </Link>
+      </div>
     </nav>
   );
 };
